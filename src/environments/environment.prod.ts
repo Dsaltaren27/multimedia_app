f2c0aa0357ca6e16775e348fb7ce4c6243007802
dsaltaren07 <dsaltaren07@gmail.com>
2025-05-09 15:39:06 -0500

    inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+  apiKey: "AIzaSyDWcDd1vGeJJu3GMFCvVnGHYwe-jpW9ftQ",
+  authDomain: "multimedia-9f454.firebaseapp.com",
+  projectId: "multimedia-9f454",
+  storageBucket: "multimedia-9f454.firebasestorage.app",
+  messagingSenderId: "555632367545",
+  appId: "1:555632367545:web:4e9f9d15c7c1a7a2e28868",
+  measurementId: "G-S9DQMGMFBS"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+  apiKey: "AIzaSyDWcDd1vGeJJu3GMFCvVnGHYwe-jpW9ftQ",
+  authDomain: "multimedia-9f454.firebaseapp.com",
+  projectId: "multimedia-9f454",
+  storageBucket: "multimedia-9f454.firebasestorage.app",
+  messagingSenderId: "555632367545",
+  appId: "1:555632367545:web:4e9f9d15c7c1a7a2e28868",
+  measurementId: "G-S9DQMGMFBS"
+}
 };
 
 /*
